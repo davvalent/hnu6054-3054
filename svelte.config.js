73444e9b1f15f08af80968613f7426bd8e784981
David Valentine <david.valentine@umontreal.ca>
2024-02-03 16:23:32 -0500

    admin: maj config

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,27 @@
 import adapter from '@sveltejs/adapter-static';
-import { mdsvex} from 'mdsvex';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-      pages: 'docs',
-      assets: 'docs',
+
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
       fallback:  undefined,
       precompress: false,
       strict: true
 		})
 	},
+
+  // extensions property tells Svelte what types of files to treat as components
   extensions: ['.svelte', '.md', '.svx'],
-  preprocess: [mdsvex({ extensions: ['.md', '.svx'] })]
+
+  preprocess: [
+    mdsvex({
+      extensions: ['.md', '.svx']
+    })
+  ]
 };
 
 export default config;
