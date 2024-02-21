eab2e7c8054361813dcec74af27362db2dfe8208
David Valentine <david.valentine@umontreal.ca>
2024-02-21 15:22:29 -0500

    tmp: to be squashed, premier jet des components

---
+++ src/lib/components/referencesLists.svelte
@@ -0,0 +1,10 @@
+<script>
+
+export let collections;
+
+</script>
+
+{#each collections.collections as collection}
+  <h1>{collection.data.name}</h1>
+  {@html collection.formattedData}
+{/each}
---
+++ src/lib/components/support.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
+<p>Ouvrir en grand</p>
+
+<iframe
+  src="../s/presentations/seance-01.html"
+  allowfullscreen="allowfullscreen"
+  allow="geolocation *; microphone *; camera *; midi *; encrypted-media *"
+  frameborder="0"
+  title="FOOBAR" />
+
+  <!-- <details>
+    <summary id="toc-title">
+      Navigation
+    </summary>
+    <nav id="TOC" role="doc-toc">
+      <ul>
+        <li><a href="#à-propos" id="toc-à-propos">À propos</a></li>
+        <li><a href="#projets-en-cours" id="toc-projets-en-cours">Projets en
+        cours</a></li>
+        <li><a href="#pour-me-joindre-davvalent" id="toc-pour-me-joindre-davvalent">Pour
+        me joindre <span class="handle">@davvalent</span></a></li>
+      </ul>
+    </nav>
+  </details> -->
