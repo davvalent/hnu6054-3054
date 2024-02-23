0a5489e8a218d90c84342b03f8d7cbb92a54cf2c
David Valentine <david.valentine@umontreal.ca>
2024-02-23 15:43:27 -0500

    dev: page pour les séances, avec support de présentation

--- src/routes/s/[slug]/+page.server.js
+++ src/routes/s/[slug]/+page.server.js
@@ -1,15 +1,14 @@
 export async function load({ params }) {
 
-	console.log("[slug]/+page.js	");
-	console.log(params.slug);
+	console.log("[slug]:", params.slug);
 
   const post = await import(`../../../lib/content/seances/${params.slug}.md`);
-	const { title, published_date } = post.metadata;
+	const { title, presentation } = post.metadata;
 	const content = post.default.render().html;
 
 	return {
 		content,
 		title,
-		published_date
+		presentation
 	};
 }
--- src/routes/s/[slug]/+page.svelte
+++ src/routes/s/[slug]/+page.svelte
@@ -3,5 +3,17 @@
 </script>
 
 <article>
+<h2>Support de présentation</h2>
+
+<p>Ouvrir en grand</p>
+
+<iframe
+  src={data.presentation}
+  loading="eager"
+  referrerpolicy="no-referrer"
+  allowfullscreen="allowfullscreen"
+  allow="encrypted-media *"
+  title={data.title} />
+
 	{@html data.content}
 </article>
