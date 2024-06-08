24a0ebdb5fbafcd3afbbdb3a20f08f206379482f
David Valentine <david.valentine@umontreal.ca>
2024-06-08 16:47:24 -0400

    maj du modele des senaces

--- src/routes/s/[slug]/+page.svelte
+++ src/routes/s/[slug]/+page.svelte
@@ -1,14 +1,17 @@
 <script>
   import { base } from '$app/paths';
 	export let data;
+  console.log("path s:", data);
 </script>
 
 <article>
+
+{#if data.presentation}
+
 <h2>Support de présentation</h2>
 
-<p>Ouvrir en grand</p>
+<p><a href="{base}{data.presentation}">Ouvrir en grand</a></p>
 
-<!-- iframe dans un if -->
 <iframe
   src={base}{data.presentation}
   loading="eager"
@@ -16,6 +19,7 @@
   allowfullscreen="allowfullscreen"
   allow="encrypted-media *"
   title={data.title} />
+{/if}
 
-	{@html data.content}
+{@html data.content}
 </article>
